3d291277623b5dd45e453a55fefe8f3c70057102
Cedric Degremont <cedric.degremont@facilecomm.com>
2021-07-30 15:36:07 +0200

    Allow time aware enumerable comparisons

--- lib/hashes_equal/enumerable_compare_helper.rb
+++ lib/hashes_equal/enumerable_compare_helper.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
 require 'hashes_equal/enumerable_diff_displayer'
+require 'hashes_equal/time_coarsener'
 
 module HashesEqual
   module EnumerableCompareHelper
+    def assert_time_aware_enumerable_equal(
+      expected,
+      actual,
+      time_granularity:,
+      verbose: true
+    )
+      time_coarsener = TimeCoarsener.new(time_granularity)
+      time_coarsened_expected = time_coarsener.call(expected)
+      time_coarsened_actual = time_coarsener.call(actual)
+
+      assert_enumerable_equal(
+        time_coarsened_expected,
+        time_coarsened_actual,
+        verbose: verbose
+      )
+    end
+
     def assert_enumerable_equal(expected, actual, verbose: true)
       displayable_diff = EnumerableDiffDisplayer.new(
         expected: expected,
